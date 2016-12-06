d375e2f6676c0246601b471c8f79e9f9bc1ed757
200313184 <glopez1206@gmail.com>
2016-12-07 00:38:48 +0100

    creacion del boton multiplicar y dividir
    codigo boton multiplicar y dividir

--- Form1.vb
+++ Form1.vb
@@ -23,4 +23,20 @@
         TextBox3.Text = c
 
     End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        a = Val(TextBox1.Text)
+        b = Val(TextBox2.Text)
+        c = a * b
+        TextBox3.Text = c
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        a = Val(TextBox1.Text)
+        b = Val(TextBox2.Text)
+        c = a / b
+        TextBox3.Text = c
+
+    End Sub
 End Class
